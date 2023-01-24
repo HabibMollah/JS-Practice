e91ac2cdb482a6f26090705c4735ced259c2400b
Habibur Rahman <hrmollah2002@gmail.com>
2023-01-24 07:29:12 +0600

    html & JS update

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-// Setup
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ *
+ * Change this code so it returns `true` when the number is greater than or equal to 10, and false if it is less than 10
+ */
 
-function lookUpProfile(name, prop) {
-  // Only change code below this line
-  // Only change code above this line
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+  if (number >= 10) {
+    return true;
+  } else {
+    return false;
+  }
 }
-
-lookUpProfile("Akira", "likes");
